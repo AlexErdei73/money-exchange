5d8ec2326c3eba7dc38d1669cb136dc6dedf7742
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-07-21 14:04:05 +0100

    Handle form submit event

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 var currencies = ["USD", "GBP", "EUR", "HUF", "DA"];
-var pricesInUSD = [1, 1.29, 1.12, 0.0030, 0.0074];
+var pricesInUSD = [1, 1.29, 1.12, 0.003, 0.0074];
 
 var inputCurrencySelectElement = document.querySelector("#input-currency");
 var outputCurrencySelectElement = document.querySelector("#output-currency");
 
 function generateOptions(currencies, selectElement) {
-    for (var i = 0; i < currencies.length; i++) {
-        var nextOptionElement = document.createElement("option");
-        nextOptionElement.value = currencies[i];
-        nextOptionElement.textContent = currencies[i];
-        if (i === 0) nextOptionElement.selected = true;
-        selectElement.appendChild(nextOptionElement);
-    }
+  for (var i = 0; i < currencies.length; i++) {
+    var nextOptionElement = document.createElement("option");
+    nextOptionElement.value = currencies[i];
+    nextOptionElement.textContent = currencies[i];
+    if (i === 0) nextOptionElement.selected = true;
+    selectElement.appendChild(nextOptionElement);
+  }
 }
 
 generateOptions(currencies, inputCurrencySelectElement);
 generateOptions(currencies, outputCurrencySelectElement);
 
-
+var formElement = document.querySelector("form");
+formElement.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("hello world");
+});
